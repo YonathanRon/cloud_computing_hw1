bafd79257a946136f0dd7b55ad9add38fe128783
YonathanRon <jonron105@gmail.com>
2023-04-29 09:47:26 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,22 +1,28 @@
 #!/bin/bash
+# Clone your application from Git
+git clone https://github.com/YonathanRon/cloud_computing_hw1.git
+
+mkdir temp
+mv main.py temp/
+sudo apt-get update -y
+sudo apt-get upgrade -y
 
 # Update system packages
-sudo yum update -y
+sudo apt install python3-pip -y
+sudo pip3 install --upgrade pip
 
 # Install necessary packages
-sudo yum install -y python3-pip sqlite
+sudo pip3 install sqlite
+sudo pip3 install fastapi
+sudo pip3 install db
+sudo pip3 install db-sqlite3
+sudo pip3 install uvicorn
 
-# Upgrade pip
-sudo -H pip3 install --upgrade pip
 
-# Install required Python packages
-sudo -H pip3 install fastapi uvicorn
-
-# Clone your application from Git
-git clone https://github.com/your_username/your_repo.git
+aws configure
 
 # Change to your application directory
-cd your_repo
+cd cloud_computing_hw1
 
 # Start the FastAPI server
-nohup uvicorn your_app:app --host 0.0.0.0 --port 80 &
+nohup uvicorn parking_lot:app --host 127.0.0.1 --port 8000 &
